bddbf1da75c639eb9c027fcfb38fa52f292adc95
Simon-HL <simon.h.l@hotmail.com>
2024-01-06 12:05:06 +0100

    basic setup of express app

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('hello')
+import express, { Request, Response } from 'express'
+
+const app = express()
+const port = Number(process.env.PORT) || 3000
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('hello world')
+})
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`)
+})
